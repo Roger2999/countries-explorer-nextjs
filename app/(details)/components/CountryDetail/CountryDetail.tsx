e5365e07787dda74ab0bd0d817a4b10923dd9f2f
Roger <rogergutierrezmartinez900@gmail.com>
2026-03-26 04:10:23 -0400

    Improve CountryDetail layout and styling

--- app/(details)/components/CountryDetail/CountryDetail.tsx
+++ app/(details)/components/CountryDetail/CountryDetail.tsx
@@ -7,10 +7,10 @@ interface Props {
 }
 const CountryDetail = ({ country }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center lg:flex-row gap-10">
+    <div className="flex flex-col items-center justify-center lg:flex-row gap-10 lg:items-start">
       {country.map((c) => (
         <Fragment key={c.cca3}>
-          <figure className="relative w-full lg:w-xl h-52 lg:h-72 overflow-hidden">
+          <figure className="relative w-full lg:max-w-xl h-52 lg:h-72 overflow-hidden">
             <Image
               src={c.flags.svg}
               priority
@@ -19,18 +19,20 @@ const CountryDetail = ({ country }: Props) => {
               className="object-container"
             />
           </figure>
-          <div className="description-container flex flex-col flex-wrap md:flex-row gap-10">
-            <p><span></span></p>
-            <p><span></span></p>
-            <p><span></span></p>
-            <p><span></span></p>
-            <p><span></span></p>
+          <div className="info-container flex flex-col gap-10">
+          <div className="description-container flex flex-col flex-wrap md:flex-row gap-10 max-w-xl">
+            <p><span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos commodi, consequuntur maxime porro quam consequatur. Unde, earum aspernatur quis cumque fugiat soluta minima, est rerum iure ea ab culpa. Atque?</span></p>
+            <p><span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, aut nihil quasi possimus obcaecati architecto, dolorum, consequuntur pariatur eveniet rerum aperiam ratione odit facere. Similique nam aspernatur eaque sequi voluptate!</span></p>
+            <p><span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, aut nihil quasi possimus obcaecati architecto, dolorum, consequuntur pariatur eveniet rerum aperiam ratione odit facere. Similique nam aspernatur eaque sequi voluptate!</span></p>
+            <p><span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, aut nihil quasi possimus obcaecati architecto, dolorum, consequuntur pariatur eveniet rerum aperiam ratione odit facere. Similique nam aspernatur eaque sequi voluptate!</span></p>
+            <p><span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, aut nihil quasi possimus obcaecati architecto, dolorum, consequuntur pariatur eveniet rerum aperiam ratione odit facere. Similique nam aspernatur eaque sequi voluptate!</span></p>
           </div>
           <div className="borders flex flex-wrap gap-5">
             <p>borders</p>
             <p>borders</p>
             <p>borders</p>
             <p>borders</p>
+          </div>
           </div>
 
         </Fragment>
